eaad42f98be8004dc4176c7d43ea29f9c2e04065
chubbybaby31 <49795066+chubbybaby31@users.noreply.github.com>
2023-12-16 14:11:36 -0500

    Update handtracker.py

--- handtracker.py
+++ handtracker.py
@@ -16,7 +16,7 @@ while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     results = hands.process(frame)
-    finger_pos = {8: None, 16: None, 4: None}
+    finger_pos = {8: None, 16: None, 4: None, 12: None}
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     face_locations = face_recognition.face_locations(rgb_frame)
     if face_locations and face_locations[0][2]>0.9:
@@ -27,15 +27,19 @@ while True:
                     h, w, c = frame.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
 
-                    if id == 8 or id == 16 or id == 4:
+                    if id == 8 or id == 16 or id == 4 or id == 12:
                         finger_pos[id] = (cx, cy)
                         cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
         try:
             mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
             distance_index = ms.distanceBetweenFingers(finger_pos[4][0], finger_pos[8][0], finger_pos[4][1], finger_pos[8][1])
-            #print(distance_index)
-            start_new_thread(ms.leftClick, (distance_index, finger_pos[4][0], finger_pos[4][1]))
+            distance_middle = ms.distanceBetweenFingers(finger_pos[4][0], finger_pos[12][0], finger_pos[4][1], finger_pos[12][1])
+            distance_ring = ms.distanceBetweenFingers(finger_pos[4][0], finger_pos[16][0], finger_pos[4][1], finger_pos[16][1])
+    
             start_new_thread(ms.updatePos, (finger_pos[4][0], finger_pos[4][1]))
+            start_new_thread(ms.leftClick, (distance_index, finger_pos[4][0], finger_pos[4][1]))
+            start_new_thread(ms.rightClick, (distance_ring, finger_pos[4][0], finger_pos[4][1]))
+            start_new_thread(ms.drag, (distance_middle, finger_pos[4][0], finger_pos[4][1]))
 
         except:
             pass
