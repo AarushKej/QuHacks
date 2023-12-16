e3ae7c3a473806ff54cf14d3c0c571bd3ea72a41
chubbybaby31 <49795066+chubbybaby31@users.noreply.github.com>
2023-12-16 14:10:15 -0500

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -3,20 +3,27 @@ import pyautogui as pg
 
 class Mouse():
     def __init__(self):
-        pass
+        self.right_down = False
+        self.left_down = False
+        self.current_pos = None
+        self.prev_pos = None
+
     def distanceBetweenFingers(self, x, X, y, Y):
         distance = math.sqrt(((x-X)**2)+((y-Y)**2))
         #print(distance)
         return distance
 
     def leftClick(self, distance, x, y):
-        if distance <= 80:
+        if distance <= 70:
             pg.click(x, y)
     
+    def drag(self, distance, x, y):
+        if  distance <= 70:
+            pg.dragTo(x, y, button="left")
+    
     def rightClick(self, distance, x, y):
-        if distance <= 80:
-            pg.click(x, y)
+        if distance <= 70:
+            pg.click(x, y, button="right")
 
     def updatePos(self, x, y):
         pg.moveTo(x, y)
-    
