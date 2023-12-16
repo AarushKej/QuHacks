3e8e85ab1015415107d7b3ee9b9017b6d6540e3b
CJ171 <74747391+CJ171@users.noreply.github.com>
2023-12-16 16:55:24 -0500

    Update handtracker.py

--- handtracker.py
+++ handtracker.py
@@ -2,14 +2,14 @@ import cv2
 import mediapipe as mp
 from mouse import Mouse
 from _thread import start_new_thread
-
+import pyautogui
 
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 ms = Mouse()
-
+windSize=pyautogui.size()
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -22,7 +22,7 @@ while True:
                 cx, cy = int(lm.x * w), int(lm.y * h)
 
                 if id == 8 or id == 16 or id == 4 or id == 12:
-                    finger_pos[id] = (cx, cy)
+                    finger_pos[id] = (cx * 2560 / 640, cy * 1440 / 480)
                     cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
     try:
         mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
@@ -32,12 +32,13 @@ while True:
 
         start_new_thread(ms.updatePos, (finger_pos[4][0], finger_pos[4][1]))
         start_new_thread(ms.leftClick, (distance_index, distance_middle, finger_pos[4][0], finger_pos[4][1]))
-        start_new_thread(ms.rightClick, (distance_ring, finger_pos[4][0], finger_pos[4][1]))
+        start_new_thread(ms.rightClick, (distance_ring, distance_middle, finger_pos[4][0], finger_pos[4][1]))
         start_new_thread(ms.volume , (distance_middle, distance_ring, distance_index))
         
     except:
         pass
     cv2.imshow("Output", frame)
+    rect = (2560,1440,2560,1440)
     key = cv2.waitKey(1)
     if key == 27:
         break
